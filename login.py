6a874a242bafedfa4658b671e0361954b2117384
anicristina42-blip <anicristina42@gmail.com>
2026-07-14 22:09:04 -0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox 
+
+janela = tk.Tk()
+janela.title("Sistema de login ")
+janela.geometry("350x200")
+
+
+janela.mainloop()
